32b384470bc93368f022204295d533550fcf2afe
Aydin <143701285+AydinDavinci@users.noreply.github.com>
2026-03-05 21:45:30 +0100

    files relocated

--- MakerSpace-app/resources/views/catalog.blade.php
+++ MakerSpace-app/resources/views/catalog.blade.php
@@ -14,6 +14,7 @@
             <div class="filtermenu">
                 <div class="searchbarfilter">
                     <input type="text" placeholder="Zoek op titel, studentnummer...">
+                    <button>Ok</button>
                 </div>
             </div>
             <div class="itemsoverview">
