cb1b704e2af752d685dcf9631c48d6a4060bf16f
Bent-Bon <143714061+Bent-Bon@users.noreply.github.com>
2026-03-05 15:06:27 +0100

    laravel init
    het skelet opzetten

--- MakerSpace-app/routes/web.php
+++ MakerSpace-app/routes/web.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/catalog', function () {
-    return view('catalog');
+Route::get('/', function () {
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
@@ -18,10 +18,3 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
-
-
-Route::get('/product-view', function () {
-    return view('Product_view');
-});
-
-// ->middleware(['auth', 'verified'])->name('product_view');
