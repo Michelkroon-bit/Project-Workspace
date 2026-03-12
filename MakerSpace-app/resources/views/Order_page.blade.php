6730f6ee7f8005fbfb40276c79327681596cf14e
jorisKuinDaVinci <99074486@mydavinci.nl>
2026-03-12 15:17:52 +0100

    css niet werkend op Order-page opgelost

--- MakerSpace-app/resources/views/Order_page.blade.php
+++ MakerSpace-app/resources/views/Order_page.blade.php
@@ -1,25 +1,20 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Order Page</title>
-    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
-
-</head>
-<body>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">Order Submitted</h2>
+    </x-slot>
+<div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
     @include('partials.header')
-
-
 
     <div class="Order_submit">
     <h1>Your order has been placed!</h1>
     <p>Estimated print time: 20 hours</p>
     <p>Thank you for your order!</p>
     
-    <p>You'll recieve an update when the printing is complete.</p>
+    <p>You'll receive an update when the printing is complete.</p>
 
-        <a href="{{ route('product.view') }}" class="HomeBtn" style="margin-top: 20px; margin-left: 10px;">Continue Shopping</a>
+        <a href="{{ route('product.view') }}" class="HomeBtn">Continue Shopping</a>
     </div>
-
-</body>
-</html>
+        </div>
+    </div>
+</x-app-layout>
